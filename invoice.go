2b702147973ec135b55321f344fc497365473687
Remi Jannel <remi@stripe.com>
2019-04-02 14:11:40 -0700

    Add support for payment_intent on invoice and default_payment_method

--- invoice.go
+++ invoice.go
@@ -87,6 +87,7 @@ type InvoiceParams struct {
 	CustomFields         []*InvoiceCustomFieldParams `form:"custom_fields"`
 	Customer             *string                     `form:"customer"`
 	DaysUntilDue         *int64                      `form:"days_until_due"`
+	DefaultPaymentMethod *string                     `form:"default_payment_method"`
 	DefaultSource        *string                     `form:"default_source"`
 	Description          *string                     `form:"description"`
 	DueDate              *int64                      `form:"due_date"`
@@ -191,6 +192,7 @@ type Invoice struct {
 	Currency                  Currency                 `json:"currency"`
 	CustomFields              []*InvoiceCustomField    `json:"custom_fields"`
 	Customer                  *Customer                `json:"customer"`
+	DefaultPaymentMethod      *PaymentMethod           `json:"default_payment_method"`
 	DefaultSource             *PaymentSource           `json:"default_source"`
 	Description               string                   `json:"description"`
 	Discount                  *Discount                `json:"discount"`
@@ -206,6 +208,7 @@ type Invoice struct {
 	NextPaymentAttempt        int64                    `json:"next_payment_attempt"`
 	Number                    string                   `json:"number"`
 	Paid                      bool                     `json:"paid"`
+	PaymentIntent             *PaymentIntent           `json:"paymentIntent"`
 	PeriodEnd                 int64                    `json:"period_end"`
 	PeriodStart               int64                    `json:"period_start"`
 	ReceiptNumber             string                   `json:"receipt_number"`
